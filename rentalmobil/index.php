65dc1da7779a650a5c98da6551067b8237b63683
ryan <ryanekafaisal@gmail.com>
2026-09-26 00:50:33 +0700

    ganti judul

--- rentalmobil/index.php
+++ rentalmobil/index.php
@@ -9,7 +9,7 @@ session_start();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard</title>
+  <title>aldo kontol</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
